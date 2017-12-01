6ca2d5b81340f092e6f08c3e3436e0f36d7e38dd
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-01 12:15:42 -0600

    12 bar blues chick, boom, chick

---
+++ twelveBarBlues.js
@@ -0,0 +1,11 @@
+function twelveBB(num){
+  for(var i = 1; i<= num; i++){
+    console.log(i);
+    console.log('chick');
+    console.log('boom');
+    console.log('chick');
+
+  }
+}
+
+twelveBB(12)
