cd4e5264d5ec0585a4deee55ed9e2bb43ed25e5e
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-17 19:58:27 -0600

    function sigma

---
+++ sigma.js
@@ -0,0 +1,11 @@
+// Implement function sigma(num) that given a number, returns the sum of all positive integers up to number (inclusive). Ex.: sigma(3) = 6 (or 1 + 2 + 3); sigma(5) = 15 (or 1 + 2 + 3 + 4 + 5).
+
+function sigma(num){
+  var sum = 0;
+  for (var i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+sigma(3)
+sigma(5)
