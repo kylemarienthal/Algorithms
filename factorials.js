bd24a81589a230903246474c87cf0e81b6fb627d
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-18 10:03:35 -0600

    factorials

---
+++ factorials.js
@@ -0,0 +1,10 @@
+// Just the Facts, ma’am. Factorials, that is. Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to number (inclusive). For example, factorial(3) = 6 (or 1 * 2 * 3); factorial(5) = 120 (or 1 * 2 * 3 * 4 * 5).
+
+function factorial(num){
+  var product = 1;
+  for (var i = 1; i <= num; i++) {
+    product = product * i;
+  }
+  return product;
+}
+factorial(3)
