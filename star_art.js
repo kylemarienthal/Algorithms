aeaca2d67d91769cf8bc52514cb9d7e5231ed50d
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-23 23:13:42 -0800

    character art has been added

--- star_art.js
+++ star_art.js
@@ -46,3 +46,22 @@ console.log(str);
 }
 drawCenteredStars();
 // (optional) Create epic text-art Empire vs. Rebellion battle scenes, with ships like (=*=)and >o<.
+
+//
+// Character Art
+// From the above, derive the following that accept and draw the given characters, not just asterisks:
+// drawLeftChars(num,char)        // For all three of these, you
+// drawRightChars(num,char)       // can safely assume that 'char'
+// drawCenteredChars(num,char)    // is a string with length 1
+function drawLeftChars(num, char){
+  var str = '';
+  for(var i = 0; i < 76; i++){
+    if(i <= num){
+      str += char;
+    }else{
+      str += " ";
+    }
+  }
+  console.log(str);
+
+}
