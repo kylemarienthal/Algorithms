5b8b6e9dd944726c1428e3a8c1f7ee3dbf28c4da
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-31 23:37:40 -0600

    threesFives starting

--- threes_and_fives.js
+++ threes_and_fives.js
@@ -2,3 +2,9 @@
 // Create threesFives() that adds values from 100 and 4000000 (inclusive) if that value is evenly divisible by 3 or 5 but not both. Display the final sum in the console.
 //
 // Second: Create betterThreesFives(start,end) that allows you to enter arbitrary start and end values for your range. Think of threesFives()as betterThreesFives(100,4000000).
+function threesFives() {
+  var sum = 0;
+  for (var i = 100; i <= 4000000; i++) {
+    if(i % )
+  }
+}
