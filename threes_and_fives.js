9af453005fa01459923b8968706b4aebcef330dc
Kyle Marienthal <kylemarienthal@gmail.com>
2017-12-31 23:50:11 -0600

    threesFives almost done

--- threes_and_fives.js
+++ threes_and_fives.js
@@ -5,6 +5,12 @@
 function threesFives() {
   var sum = 0;
   for (var i = 100; i <= 4000000; i++) {
-    if(i % )
+    if(i % 3 === 0 && i % 5 === 0){
+      continue;
+    }
+    else if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
   }
+  console.log(sum);
 }
