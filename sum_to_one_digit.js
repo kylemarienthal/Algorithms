ccd3f5ad74db1d2ec48bdd49e88a1da8255d754d
Kyle Marienthal <kylemarienthal@gmail.com>
2018-01-03 23:36:31 -0600

    sum to one number still not working with recursion 2

--- sum_to_one_digit.js
+++ sum_to_one_digit.js
@@ -2,13 +2,12 @@
 // Kaitlin sees beauty in numbers, but also believes that less is more. Implement sumToOne(num) that sums a given integer’s digits repeatedly until the sum is only one digit. Return that one-digit result. Example: sumToOne(928) returns 1, because 9+2+8 = 19, then 1+9 = 10, then 1+0 = 1.
 //
 function sumToOne(num){
-  var sum1 = 0;
-  if(num.length === 1){
-    return num
-  }else{
-      for(var i = 0; i < num.length; i++){
-        sum1 += num[i];
+  var sum1 = num;
+  if(sum1.length === 1){
+    return sum1;
+  }
+  for(var i = 0; i < sum1.length; i++){
+        sum1 += sum1[i];
       }
   sumToOne(sum1);
-  }
 }
