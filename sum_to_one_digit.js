085667ddcbb95fac8c0797dbe9ec012590e0894d
Kyle Marienthal <kylemarienthal@gmail.com>
2018-01-08 17:41:35 -0600

    sum to one number started using modulus 10

--- sum_to_one_digit.js
+++ sum_to_one_digit.js
@@ -1,8 +1,9 @@
 // Sum to One Digit
 // Kaitlin sees beauty in numbers, but also believes that less is more. Implement sumToOne(num) that sums a given integer’s digits repeatedly until the sum is only one digit. Return that one-digit result. Example: sumToOne(928) returns 1, because 9+2+8 = 19, then 1+9 = 10, then 1+0 = 1.
 //
+
+//use % 10 for the length of num after each iteration
 function sumToOne(num){
-  var sum1 = num;
   if(sum1.length === 1){
     return sum1;
   }
