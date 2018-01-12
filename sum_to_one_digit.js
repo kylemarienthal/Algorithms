4e013ba09b3f04276cc3903fe966323a27b99da5
Kyle Marienthal <kylemarienthal@gmail.com>
2018-01-11 21:23:41 -0600

    sum to one completed

--- sum_to_one_digit.js
+++ sum_to_one_digit.js
@@ -1,6 +1,5 @@
 // Sum to One Digit
 // Kaitlin sees beauty in numbers, but also believes that less is more. Implement sumToOne(num) that sums a given integer’s digits repeatedly until the sum is only one digit. Return that one-digit result. Example: sumToOne(928) returns 1, because 9+2+8 = 19, then 1+9 = 10, then 1+0 = 1.
-//
 
 // 1) Create a "total" variable, and set a "working" variable to the value you want to sum.
 // 2) Create a loop, and set the total variable to zero
@@ -9,7 +8,7 @@
 // 5) If the working variable is greater than zero then go back round the loop at (3)
 // 6) If the total variable is less than ten, you have the value.
 // 7) Otherwise, set the working variable to the total variable, and go back round the loop at (2)
-// 
+//
 // split the number apart into an array
 // loop through the array and parseint each index to add it to the 'sum' if sum < 10 return it
 // otherwise run the function on that number again
